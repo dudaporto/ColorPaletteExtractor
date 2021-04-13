711890979eab597633131dff8a45a05c97019d0d
Maria Eduarda Porto <dudabatistaporto@gmail.com>
2021-04-12 21:15:43 -0300

    Init project + installing pods

---
+++ ColorPaletteExtractor/ColorPaletteExtractorTests/ColorPaletteExtractorTests.swift
@@ -0,0 +1,33 @@
+//
+//  ColorPaletteExtractorTests.swift
+//  ColorPaletteExtractorTests
+//
+//  Created by Maria Eduarda Porto on 12/04/21.
+//
+
+import XCTest
+@testable import ColorPaletteExtractor
+
+class ColorPaletteExtractorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ ColorPaletteExtractor/Pods/Target Support Files/Pods-ColorPaletteExtractorTests/Pods-ColorPaletteExtractorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ColorPaletteExtractorTests : NSObject
+@end
+@implementation PodsDummy_Pods_ColorPaletteExtractorTests
+@end
